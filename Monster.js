c4eb2764af9809b01614d4ffb0e192b5eb4edc42
matrikaGhosh <76779639+matrikaGhosh@users.noreply.github.com>
2021-03-23 14:26:49 +0530

    Add files via upload

---
+++ Monster.js
@@ -0,0 +1,18 @@
+class Monster {
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic: true,
+            restitution: 0.5,
+            friction: 1.0,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body)
+    }
+    display() {
+        push();
+        translate(this.body.position.x, this.body.position.y)
+        pop();
+    }
+}
---
+++ Superhero.js
@@ -0,0 +1,22 @@
+class Superhero{
+constructor(x,y,width,height){
+var options = {
+    isStatic:false,
+    restitution:0,
+    friction:1,
+    density:1.2,
+}
+this.body = Bodies.rectangle(x,y,width,height,options);
+this.width = width;
+this.height = height;
+World.add(world,this.body)
+
+}
+display(){
+var pos = this.body.pos;
+var angle = this.body.angle
+push();
+translate(this.body.position.x,this.body.position.y)
+pop();
+}
+}
